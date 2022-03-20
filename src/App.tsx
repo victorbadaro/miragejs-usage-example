aac577d65a22dc365d5ca32bc8cb2cb96f5d12ee
Victor <victor.badaro@fatec.sp.gov.br>
2022-03-20 17:34:42 -0300

    feat: add some basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <h1>Hello, MirageJS!</h1>
+    <>
+      <GlobalStyle />
+      <h1>Hello, MirageJS!</h1>
+    </>
   );
 }
