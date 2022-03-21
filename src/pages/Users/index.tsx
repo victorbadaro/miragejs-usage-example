85e86f08c9bc30d9c2bb3e947223a3156a11297c
Victor <victor.badaro@fatec.sp.gov.br>
2022-03-21 07:55:53 -0300

    feat: get users and show on screen

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
+type User = {
+    id: number;
+    name: string;
+}
+
 export function Users() {
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const { data } = await api.get('/users');
+            
+            setUsers(data.users);
+        })();
+    }, []);
+
     return (
-        <h1>Users</h1>
+        <>
+            <h1>Users</h1>
+
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </>
     );
 }
