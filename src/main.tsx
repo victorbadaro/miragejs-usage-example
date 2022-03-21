07637783e7baf461197a632dc301b7e923627512
Victor <victor.badaro@fatec.sp.gov.br>
2022-03-21 07:29:01 -0300

    feat: add endpoint to get users

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,18 @@ createServer({
   routes() {
     // API Prefix
     this.namespace = 'api';
+
+    // API Endpoints
+    this.get('/users', () => {
+      const users = [
+        { id: 1, name: 'Victor Badaró' },
+        { id: 2, name: 'Miryam Santana' },
+        { id: 3, name: 'Lívia Rodrigues' },
+        { id: 4, name: 'Valdir Xavier' }
+      ];
+
+      return users;
+    });
   }
 });
 
