3ad4b1a5aa62c3287dd72cb3c6bbb896486a68b7
Victor <victor.badaro@fatec.sp.gov.br>
2022-03-21 07:22:28 -0300

    feat: add initial mirage config to create server

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
+import { createServer } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+
+createServer({
+  routes() {
+    // API Prefix
+    this.namespace = 'api';
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
