4aa5cec04a40af0c25fcd3cbdb1d8d7385af62be
Victor <victor.badaro@fatec.sp.gov.br>
2022-03-21 08:37:30 -0300

    feat: add event to create a user

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { api } from '../../services/api';
 
 type User = {
-    id: number;
+    id: number | string;
     name: string;
 }
 
 export function Users() {
     const [users, setUsers] = useState<User[]>([]);
+    const [name, setName] = useState('');
 
     useEffect(() => {
         (async () => {
@@ -16,6 +17,13 @@ export function Users() {
             setUsers(data.users);
         })();
     }, []);
+
+    async function handleFormSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        const { data } = await api.post('/users', { name });
+        setUsers([ ...users, data.user ]);
+    }
 
     return (
         <>
@@ -26,6 +34,17 @@ export function Users() {
                     <li key={user.id}>{user.name}</li>
                 ))}
             </ul>
+
+            <form action="post" onSubmit={handleFormSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    onChange={event => setName(event.target.value)}
+                    value={name}
+                />
+                <button type="submit">Add user</button>
+            </form>
         </>
     );
 }
